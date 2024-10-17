bc927fa6ee446e7cedfa160eed6ec833e3017bee
Waseem2209 <waseemraza2209@gmail.com>
2024-10-17 17:25:32 +0500

    new add

--- app/components/MainBox/mainBox.js
+++ app/components/MainBox/mainBox.js
@@ -20,40 +20,7 @@ const MainBox = () => {
           <h1 className="font-bold text-4xl mt-16">Buff OP_CAT</h1>
           <p className="mt-2 text-base">Lock your token and earn reward</p>
         </div>
-
-        <div className="lg:w-[900px] py-5 box-border bg-[#301B00E5] m-auto rounded-lg mt-10">
-          <div className="w-full md:w-[95%] h-[60px] bg-gradient-to-r from-[#EFCB97] to-[#F3933F]  rounded-lg m-auto ">
-            <ol className="flex justify-around items-center h-full font-medium cursor-pointer text-[.7em] sm:text-[.8em] lg:text-[1em]">
-              <li
-                className={newroute == "Dashboard" ? "font-semibold" : ""}
-                onClick={() => setnewroute("Dashboard")}
-              >
-                Dashboard
-              </li>
-              <li
-                className={newroute == "Localtoken" ? "font-semibold" : ""}
-                onClick={() => setnewroute("Localtoken")}
-              >
-                Lock Token
-              </li>
-              <li>Coin Reward</li>
-              <li>Leaderboard</li>
-              <li onClick={() => setnewroute("trendingtoken")}>
-                Trending Token
-              </li>
-            </ol>
-          </div>
-
-          {newroute == "Dashboard" ? (
-            <Dashboard />
-          ) : newroute == "trendingtoken" ? (
-            <TrendingToken />
-          ) : (
-            <Locktoken />
-          )}
-        </div>
-      <Footer />
-      <div className="lg:w-[800px] py-5 box-border bg-[#301B00E5] m-auto rounded-lg mt-10">
+        <div className="lg:w-[800px] py-5 box-border bg-[#301B00E5] m-auto rounded-lg mt-10">
       <div className="w-full md:w-[95%] h-[60px] bg-gradient-to-r from-[#EFCB97] to-[#F3933F]  rounded-lg m-auto ">
           <ol className="flex justify-around items-center h-full font-medium cursor-pointer text-[.7em] sm:text-[.8em] lg:text-[1em]">
             <li className={newroute == 'Dashboard' ? 'font-semibold' : ''} onClick={() => setnewroute('Dashboard')}>Dashboard</li>
@@ -68,6 +35,9 @@ const MainBox = () => {
         newroute == 'Dashboard' ? <Dashboard/> : newroute == 'Leaderboard' ? <Leaderboard/> : newroute == 'CoinReward' ? <Coinreward/> : <Locktoken/>
       }
       </div>
+      
+      <Footer />
+      
     </div>
     </div>
     
