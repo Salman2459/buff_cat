2266e9f1cfb3789ddc49564e1ffa2ba6ff1ae436
Salman2459 <120258780+Salman2459@users.noreply.github.com>
2024-12-04 16:21:27 +0500

    New

--- app/components/MainFooter/footer.js
+++ app/components/MainFooter/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='bg-[#291B0E] mt-10'>
       <div className='flex'>
         <div className='flex-1 flex justify-center mt-5 text-white items-center'><Link href={'/MainHome'}>Open App</Link></div>
-        <div className='flex-1 flex justify-center mt-5 text-white items-center'><Link href={'https://x.com/BuffCatOfficial'}><img src='/mainsX.png' /></Link></div>
+        <div className='flex-1 flex justify-center mt-5 text-white items-center'><Link href={'https://x.com/BuffCatOfficial'}><img src='./mainX.png' className='w-[50px] h-[50px] bg-white rounded-lg' /></Link></div>
         <div className='flex-1 flex justify-center mt-5 text-white items-center' ><Link href={'https://docs.buffcat.org/ '}>Docs</Link></div>
 
       </div>
