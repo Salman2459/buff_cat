0616c7a5d602104b7f623f4aa1aa56a0bade1a07
Salman2459 <120258780+Salman2459@users.noreply.github.com>
2024-12-07 21:02:17 +0500

    *

--- app/page.js
+++ app/page.js
@@ -1,18 +1,11 @@
-import HeroSection from './components/HeroSection/HeroSection'
-import Navbar from './components/HomeNavbar/Navbar'
-import HomeTable from './components/HomeTable/HomeTable'
-import Footer from './components/MainFooter/footer'
-import Working from './components/Working/Working'
+import WalletDisconnected from './components/WalletDisconnected/walletDisconnected'
+
 
 const Home = () => {
 
-   return <div className='bg-[#000000] w-full min-h-screen  Dosis'>
-      <Navbar />
-      <HeroSection />
-      <Working />
-      <HomeTable />
-      <Footer/>
-   </div>
+   return <>
+      <WalletDisconnected />
+   </>
 }
 
 export default Home
